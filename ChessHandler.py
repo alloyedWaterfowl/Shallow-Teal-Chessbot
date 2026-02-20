d1a00784055e64555eb50e4b400a89026fd2e823
danielgriffor-cmyk <danielgriffor@gmail.com>
2026-02-20 13:01:07 -0500

    Change Initial Position

--- ChessHandler.py
+++ ChessHandler.py
@@ -8,8 +8,8 @@ import ComplexChessBot
 import RandomChessBot
 import CheckChessBot
 
-white_bot = ComplexChessBot.Bot(chess.WHITE, depth=3)
-black_bot = ComplexChessBot.Bot(chess.BLACK, depth=3)
+white_bot = ComplexChessBot.Bot(color = chess.WHITE, depth=2)
+black_bot = ComplexChessBot.Bot(color = chess.BLACK, depth=3, qsearch = False)
 
 gui = chessGUI(white_player=white_bot, black_player=black_bot)
 gui.move_time = 100
